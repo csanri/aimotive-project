03511cd7484724d688794f8f118a08cbaa0122ca
csanri <richard.csanadi@gmail.com>
2026-03-31 13:43:55 +0200

    data_loader több kamera nézettel

--- main.py
+++ main.py
@@ -7,17 +7,14 @@ if __name__ == "__main__":
 
     # Példa adatok
     img_data = ImgData(
-        folder="/mnt/oldssd/train/",
-        section_id="highway",
-        frame_id="20210401-073402-00.18.00-00.18.15@Jarvis",
+        folder="/mnt/oldssd/train/highway/",
+        section_id="20210401-073402-00.18.00-00.18.15@Jarvis",
+        frame_id="_0016239",
         logger=logger
     )
 
     calibration_data = img_data.load_data()
-    camera_data = img_data.load_camera(
-        camera="B_MIDRANGECAM_C",
-        img_name="B_MIDRANGECAM_C_0016239.jpg"
-    )
+    camera_data = img_data.load_camera()
 
     print(f"Calibration data: {calibration_data}")
     print(f"Camera data: {camera_data}")
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,11 +1,17 @@
 import json
 import logging
-from pathlib import Path
-from typing import Any
-import numpy as np
-from PIL import Image
-from torchvision import transforms
 import os
+import numpy as np
+import cv2
+
+from typing import Any
+
+CAMERA_TABLE = {
+    "back_camera":  "B_MIDRANGECAM_C",       # back camera
+    "front_camera": "F_MIDLONGRANGECAM_CL",  # front camera CL
+    "left_camera":  "M_FISHEYE_L",           # left camera
+    "right_camera": "M_FISHEYE_R"            # right camera
+}
 
 
 class ImgData:
@@ -34,53 +40,74 @@ class ImgData:
         Return:
             dict: tartalmazza az összes kalibrációs adatot
         """
-        path = Path(self.folder
-                    + '/'
-                    + self.section_id
-                    + '/'
-                    + self.frame_id
-                    + "/sensor/calibration/calibration.json"
-                    )
+        path = os.path.join(
+            self.folder,
+            self.section_id,
+            "sensor",
+            "calibration",
+            "calibration.json"
+        )
         self.logger.info(f"Loading calibration parameters from: '{path}'")
 
-        if not path.exists():
+        if not os.path.exists(path):
             self.logger.error(f"JSON file does not exist: '{path}'")
             raise FileNotFoundError
 
-        with path.open() as f:
+        with open(path, 'r') as f:
             data = json.load(f)
 
         self.logger.info(f"Calibration parameters loaded: '{path}'")
 
         return data
 
-    def load_camera(self, camera: str, img_name: str) -> np.ndarray:
+    def load_camera(self) -> list[np.ndarray]:
         """
-        Bertölti az adott kamera adott képét
+        Betölti a kamera képeket
         Return:
-            np.ndarray: kamera képe Numpy arrayként
-            (Height, Width, Channel)
+            list[np.ndarray]: 4 kamera kép adott frame_id-hoz
         """
-        path = Path(self.folder
-                    + '/'
-                    + self.section_id
-                    + '/'
-                    + self.frame_id
-                    + "/sensor/camera/"
-                    + camera
-                    + '/'
-                    )
-        self.logger.info(f"Loading camera data from: '{path}'")
+        path = os.path.join(
+            self.folder,
+            self.section_id,
+            "sensor/camera"
+        )
 
-        if not path.exists():
+        if not os.path.exists(path):
             self.logger.error(f"Camera path does not exist: '{path}'")
             raise FileNotFoundError
 
-        img = Image.open(os.path.join(path, img_name))
+        self.logger.info(f"Loading camera data from: '{path}'")
 
-        # Transzformálni kell a képet torch tenzorból numpy arraybe
-        transform = transforms.ToTensor()
-        tensor = transform(img)
-        array = tensor.numpy().transpose(1, 2, 0)  # Height, Width, Channel
+        self.back_camera = cv2.imread(
+            os.path.join(
+                path,
+                CAMERA_TABLE["back_camera"],
+                CAMERA_TABLE["back_camera"] + self.frame_id + ".jpg"
+            )
+        )
 
-        return (array * 255).astype(np.uint8)
+        self.front_camera = cv2.imread(
+            os.path.join(
+                path,
+                CAMERA_TABLE["front_camera"],
+                CAMERA_TABLE["front_camera"] + self.frame_id + ".jpg"
+            )
+        )
+
+        self.left_camera = cv2.imread(
+            os.path.join(
+                path,
+                CAMERA_TABLE["left_camera"],
+                CAMERA_TABLE["left_camera"] + self.frame_id + ".jpg"
+            )
+        )
+
+        self.right_camera = cv2.imread(
+            os.path.join(
+                path,
+                CAMERA_TABLE["right_camera"],
+                CAMERA_TABLE["right_camera"] + self.frame_id + ".jpg"
+            )
+        )
+
+        return [self.back_camera, self.front_camera, self.left_camera, self.right_camera]
